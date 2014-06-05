571df542c562e583a0d12d54a21f9da7f2b9c48d
Maike Jordan <>
2014-06-05 18:45:36 -0300

    Modelagem do Escopo - Inclusão de Entidades

--- TrabalhoFinalAS/src/java/br/edu/ifnmg/tads/as/DomainModel/Administrador.java
+++ TrabalhoFinalAS/src/java/br/edu/ifnmg/tads/as/DomainModel/Administrador.java
@@ -8,9 +8,6 @@ package br.edu.ifnmg.tads.as.DomainModel;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 /**
  *
